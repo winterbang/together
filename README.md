d57cfa0cb94e1ffc02256cb1ae83ce91683bec87
winter zhang <winter.zhang@9street.org>
2017-04-01 23:14:24 +0800

    对接提交接口

--- README.md
+++ README.md
@@ -3,4 +3,6 @@
 `icon`  http://www.opendigg.com/tags/wechat-app
 `小程序组件库` http://www.opendigg.com/tags/wechat-app
 
+阿里icon http://iconfont.cn/
+
 `其他` http://94pm.com/2518.html
--- pages/publish/publish.js
+++ pages/publish/publish.js
@@ -6,12 +6,12 @@ Page({
   data: {
     title: "",
     departurePlace: "",
-    startData: '出发时间',
+    startData: '',
     destination: "",
-    endData: '离开时间',
+    endData: '',
     byWayOf: [],
     peopleLimit:[1, 2],
-    contactWay: {},
+    contactWays: [],
     desc: "",
     userInfo: {},
     config: [{
@@ -32,7 +32,6 @@ Page({
     })
     console.log(api.auth().currentUser.uid)
     let wilddog = app.API()
-    console.log(wilddog.sync(), '==========api.sync=========')
     var sessionsRef = wilddog.sync().ref("sessions");
     var mySessionRef = sessionsRef.push();
     mySessionRef.onDisconnect().update({
@@ -88,23 +87,75 @@ Page({
       byWayOf: locations
     })
   },
+  addContactWay: function(e) {
+    var contactWays = this.data.contactWays;
+    contactWays.push({type:"", value: ""});
+    this.setData({
+      contactWays: contactWays
+    })
+  },
+  choiceContactType: function(e) {
+    var that = this;
+    var items = {qq: 'QQ', wechat: '微信', phoneNumber: '手机号'}
+    var targetId = e.target.dataset.id;
+    var contactWays = this.data.contactWays;
+    wx.showActionSheet({
+      itemList: Object.values(items),
+      success: function(res) {
+        contactWays[targetId].type = Object.keys(items)[res.tapIndex]
+        that.setData({
+          contactWays: contactWays
+        })
+      },
+      fail: function(res) {
+        console.log(res.errMsg)
+      }
+    })
+  },
+  removeContactWay: function(e) {
+    var targetId = e.target.dataset.id;
+    var contactWays = this.data.contactWays;
+    contactWays.splice(targetId, 1);
+    this.setData({
+      contactWays: contactWays
+    })
+  },
+  getData: function(e) {
+    var keys = e.target.dataset.key.split('.');
+    var oldData = this.data;
+    var lastNest = oldData;
+    keys.forEach(function(v, i){
+      if(i == keys.length-1){
+        lastNest[v] = e.detail.value
+      }else {
+        v = parseInt(v)||v
+        lastNest = lastNest[v]
+      }
+    })
+    this.setData({
+      [keys[0]]: oldData[keys[0]]
+    })
+  },
   publish: function() {
-    const {title, departurePlace, startData, destination, endData, byWayOf, peopleLimit, contactWay, desc} = this.data
+    const {title, departurePlace, startData, destination, endData, byWayOf, peopleLimit, contactWays, desc} = this.data
     let uid = api.auth().currentUser.uid
-    let journey = {uid, title, departurePlace, startData, destination, endData, byWayOf, peopleLimit, contactWay, desc}
+    let journey = {uid, title, departurePlace, startData, destination, endData, byWayOf, peopleLimit, contactWays, desc}
+    console.log(journey)
     api.sync().ref("journeys").push(journey)
       .then(function(newRef){
          // newRef 的地址类似下面：
          // https://<appId>.wilddogio.com/city/-JmRhjbYk73IFRZ7
          console.info(newRef.toString());
          console.info(newRef.key());
-        //  api.sync().ref(`userinfo/${uid}/journeys`).on('child_added',function(snapshot,prev){
-        //    journeys.push(snapshot.val())
-        //  });
-         api.sync().ref(`userinfo/${uid}/journeys/0`).set(newRef.key())
+         let lastJourneyId
+         api.sync().ref(`userinfo/${uid}/journeys`)
+          .limitToLast(10).on('child_added',function(snapshot,prev){
+           lastJourneyId = snapshot.key()
+         });
+         api.sync().ref(`userinfo/${uid}/journeys/${lastJourneyId+1}`).set(newRef.key())
       })
       .catch(function(err){
          console.info('remove node failed', err.code, err);
       });
-    }
+  }
 })
